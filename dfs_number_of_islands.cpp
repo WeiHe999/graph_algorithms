7100517bcd4827cc4e539ad8a217a5eed9e3a390
WeiHe999 <86860078+WeiHe999@users.noreply.github.com>
2021-07-30 15:35:06 -0400

    Update dfs_number_of_islands.cpp

--- dfs_number_of_islands.cpp
+++ dfs_number_of_islands.cpp
@@ -11,26 +11,22 @@ input
 */
 #include <iostream>
 #include <vector>
-#include <unordered_map>
 #include <algorithm>
 using namespace std;
-
-
-void dfs(vector< vector<int> > graph, vector <pair<int, int> > &visited, pair<int, int> start_node, int rows, int cols)
+void dfs(vector <vector <int> > graph, pair <int, int> start_node, vector <pair <int, int> > &visited, int rows, int cols)
 {
     if (find(visited.begin(), visited.end(), start_node) == visited.end())
     {
-        visited.emplace_back(start_node);
-        // find the neighbors
+        visited.push_back(start_node);
         int x = start_node.first;
         int y = start_node.second;
-        vector <pair<int, int> > neighbors = {{x + 1, y}, {x - 1, y}, {x, y + 1}, {x, y - 1}};
-        for (auto a : neighbors)
+        vector <pair <int, int> > neighbours = {{x + 1, y}, {x - 1, y}, {x, y + 1}, {x, y - 1}};
+        for (auto a : neighbours)
         {
-            if (a.first>=cols || a.second>=rows || a.first<0 || a.second<0) continue;
-            if (graph[a.first][a.second]==0) continue;
-            // dfs on neighbor
-            dfs(graph, visited, a, rows, cols);
+            
+            if (a.first < 0 || a.second < 0 || a.first >= rows || a.second >= cols) continue;
+            if (graph[a.first][a.second] == 0) continue;
+            dfs(graph, a, visited, rows, cols);
         }
     }
 }
@@ -40,59 +36,44 @@ int main()
     int rows, cols;
     cin >> rows >> cols;
     cin.ignore();
-    
-    vector< vector<int> > graph;
+    vector <vector <int> > graph;
+    vector <int> tmp;
     string line;
-    for (int i = 0; i < rows; i++)
+    for (int x = 0; x < rows; x++)
     {
         getline(cin, line);
-        vector<int> row_vec;
-        for(int j=0; j<line.size(); j++)
+        tmp = {};
+        for (int a = 0; a < cols; a++)
         {
-            if(line[j]=='+') row_vec.push_back(1);
-            else if (line[j]=='-') row_vec.push_back(0);
-            else continue;
+            if (line[a] == '-') tmp.push_back(0);
+            if (line[a] == '+') tmp.push_back(1);
         }
-        graph.push_back(row_vec);
-
+        graph.push_back(tmp);
     }
-    
-    //print the graph
-    cout << "Grid matrix:" << endl;
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < cols; j++)
-        {
-            cout << graph[i][j] << " ";
-        }
-        cout << endl;
-    }
-    
-    // dfs
-    vector <pair<int, int> > global_visited = {};
-    vector <pair<int, int> > local_visited = {};
-    int cnt = 0;
-
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < cols; j++)
-        {
-            pair<int, int> s = {i, j};
-            if (graph[i][j]==1 && find(global_visited.begin(), global_visited.end(), s) == global_visited.end())
-            {
-                cnt ++;
-                local_visited = {};
-                dfs(graph, local_visited, s, rows, cols);
-                global_visited.insert(global_visited.end(), local_visited.begin(), local_visited.end() );
-            }
-            
-        }
-    }
-
-    // for (auto x: global_visited)
+    // cout << "Matrix:" << endl;
+    // for (auto b : graph)
     // {
-    //     cout << x.first << ", " << x.second << endl;
+    //     for (auto c : b)
+    //     {
+    //         cout << c << " ";
+    //     }
+    //     cout << endl;
     // }
-    cout << "The number of islands is: " << cnt << endl;
-
+    vector <pair <int, int> > global_visited;
+    vector <pair <int, int> > visited;
+    int s = 0;
+    pair <int, int> p;
+    for (int b = 0; b < rows; b++)
+    {
+        for (int c = 0; c < cols; c++)
+        {
+            p = {b, c};
+            if (graph[b][c] == 0 || find(global_visited.begin(), global_visited.end(), p) != global_visited.end()) continue;
+            dfs(graph, p, visited, rows, cols);
+            global_visited.insert(global_visited.end(), visited.begin(), visited.end());
+            visited = {};
+            s++;
+        }
+    }
+    cout << s << endl;
 }
