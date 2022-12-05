e80bf66f60ff4e674e71ecb8189c66997055deeb
WeiHe999 <86860078+WeiHe999@users.noreply.github.com>
2022-12-04 21:16:02 -0500

    Update 0_prim_road_network.cpp

--- 0_prim_road_network.cpp
+++ 0_prim_road_network.cpp
@@ -1,35 +1,86 @@
-//Road Network
+// dmoj: Road Network
+
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-typedef pair<double, int> pdi;
-const int MM = 1003;
-int N, M, x[MM], y[MM]; double dis[MM]; bool vis[MM]; vector<pdi> adj[MM];
-int main(){
-    cin >> N >> M;
-    for(int i=1; i<=N; i++){
-        cin >> x[i] >> y[i];
-        for(int j=1; j<i; j++){
-            double d = hypot(x[i] - x[j], y[i] - y[j]);
-            adj[i].push_back({d, j});  adj[j].push_back({d, i});
+
+long long distance(long long x1, long long y1, long long x2, long long y2)
+{
+    return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
+}
+
+
+// node_id from 1 to n
+double prim(unordered_map <long long, unordered_map<long long, long long> > &graph, int n)
+{
+    // PRIM for MST
+    vector<long long> dist(n+1, LLONG_MAX), parents(n+1, -1);
+    vector<bool> vis(n+1);
+    double tot_dist = 0;
+    priority_queue<pair<long long, long long> > min_q; //min_queue={dist, node_id}
+    // push the first node
+    dist[1] = 0;
+    min_q.push({0, 1});
+    while(!min_q.empty())
+    {
+        pair<long long, long long> min_n = min_q.top();
+        long long cur_node = min_n.second;
+        min_q.pop();
+        if (vis[cur_node]) continue;
+        vis[cur_node] = true;
+        for (auto x: graph[min_n.second])
+        {
+            long long child_node = x.first;
+            if ((!vis[child_node]) & (graph[cur_node][child_node] < dist[child_node]))
+            {
+                // update dist and parents
+                dist[child_node] = graph[cur_node][child_node];
+                parents[child_node] = cur_node;
+                // push into queue
+                min_q.push({-1*graph[cur_node][child_node], child_node});
+            }
         }
     }
-    for(int i=1, u, v; i<=M; i++) {
-        cin >> u >> v; adj[u].push_back({0, v}); adj[v].push_back({0, u});
+    for (int i=1; i<=n; i++) 
+    {
+        tot_dist += sqrt(dist[i]);
     }
-    priority_queue<pdi, vector<pdi>, greater<pdi> > q;
-    fill(dis, dis+MM, 1e18);
-    dis[1] = 0.0;  q.push({0.0, 1});
-    while(!q.empty()){
-        double d = q.top().first; int u = q.top().second; q.pop();
-        if(vis[u]) continue;
-        vis[u] = true;
-        for(auto e : adj[u]){
-            double w = e.first; int v = e.second;
-            if(!vis[v] && dis[v] > w) { dis[v] = w; q.push({dis[v], v}); }
+    return tot_dist;
+}
+
+
+int main()
+{
+    cin.tie(0); cout.tie(0); cin.sync_with_stdio(0);
+    long long n, m, a, b;
+    cin >> n >> m;
+    unordered_map <long long, pair <long long, long long> > m1;
+    unordered_map <long long, unordered_map<long long, long long> > graph;//{start_node: {end_node: dist}}
+    for (long long x = 1; x <= n; x++)
+    {
+        cin >> a >> b;
+        m1[x] = {a, b};
+    }
+    // completely connected graph
+    for (long long x = 1; x <= n; x++)
+    {
+        for (long long y = x+1; y <= n; y++)
+        {
+            long long d = distance(m1[x].first, m1[x].second, m1[y].first, m1[y].second);
+            graph[x][y] = d;
+            graph[y][x] = d;
         }
     }
-    double ans = 0;
-    for(int i=1; i<=N; i++) ans += dis[i];
-    cout << fixed << setprecision(2) << ans << endl;
+    // set the cost for the existing edges to 0
+    for (long long x = 0; x < m; x++)
+    {
+        cin >> a >> b;
+        graph[a][b] = 0;
+        graph[b][a] = 0;
+    }
+    
+    // PRIM for MST
+    double ans = prim(graph, n);
+
+ 
+    cout << fixed << setprecision(2) << ans << "\n";
 }
