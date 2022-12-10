9385fcb53a5bd9b91e7c2ed47ee61221e1f6db3b
WeiHe999 <86860078+WeiHe999@users.noreply.github.com>
2022-12-10 15:04:59 -0500

    Update 0_0graph_templates.cpp

--- 0_0graph_templates.cpp
+++ 0_0graph_templates.cpp
@@ -107,9 +107,11 @@ int bfs(vector <int> start_nodes, unordered_set <int> end_nodes, int n)
 }
 
 // *************** DFS ********************
-// dfs on unweighted graph
+// dfs on an unweighted graph
+// initialize the two vectors as below:
 vector <bool> vis(n + 1, false);
-vector<int> dist(n+1);
+vector<int> dist(n+1, 0);
+
 void dfs(unordered_map <int, unordered_set <int> > &graph, int cur_node, vector <bool> &vis, vector<int> &dist)
 {
     vis[cur_node] = true;
@@ -118,7 +120,7 @@ void dfs(unordered_map <int, unordered_set <int> > &graph, int cur_node, vector 
         if (!vis[x])
         {
             dist[x] = dist[cur_node] + 1;
-            dfs(graph, x, vis, cur);
+            dfs(graph, x, vis, dist);
         }
     }
 }
@@ -152,3 +154,7 @@ void floyd_warshall(unordered_map<int, unordered_map<int, int> > graph, vector <
         }
     }
 }
+
+// *************** Tree ********************
+
+
